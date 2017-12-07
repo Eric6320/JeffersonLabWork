fb4a4da74c6c914c1e486e2bddbda29317861ffb
Eric Thompson <erict@opsl00.acc.jlab.org>
2017-12-07 11:33:51 -0500

    Finished transitioning up to setup.sh

--- functionsDir/setup.sh
+++ functionsDir/setup.sh
@@ -8,14 +8,12 @@ set VERTICLE = $4
 set S = $5
 
 # Set static twiss parameters and s coordinate of BPM and two chosen quadrupoles
-grep 'IPM' "$RDPATH/information.twiasc" | awk -v S=$S '{if ($1 >= S) print $2" "$1}' >! "downstreamBPM.dat"
+grep 'IPM' $RDPATH/information.twiasc | awk -v S=$S '{if ($1 >= S) print $2" "$1}' >! "downstreamBPM.dat"
 
 # Generate a unit circle with N evenly spaced data points - circle$N.dat
-if (`ls | grep -c "circle$N.dat"` == 0) then
-	echo "setup.sh - Generating unit circle";
-	unitCircle.sh $N
-endif
+echo "setup.sh - Generating unit circle";
+$FPATH/unitCircle.sh $N
 
 # Transform the points on the unit circle to a betatron ellipse at the specified BPM using the modified lattice- modifiedEllipseOne.dat
 echo "setup.sh - Performing inverse floquet transformation on modified beamline"
-floquet.sh "circle$N.dat" "modifiedEllipseOne.dat" $BPMONE $VERTICLE "inverse"
+$FPATH/floquet.sh "circle$N.dat" "modifiedEllipseOne.dat" $BPMONE $VERTICLE "inverse"
