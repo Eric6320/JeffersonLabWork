b9a3c229d8206fe97021ca2284b1693ab594682f
Eric Thompson <erict@opsl00.acc.jlab.org>
2017-12-09 10:42:17 -0500

    not sure if anything changed, but just to be sure

---
+++ functionsDir/setEnv.sh
@@ -0,0 +1,10 @@
+#!/bin/tcsh
+
+setenv FPATH ~/git/JeffersonLabWork/functionsDir
+setenv RDPATH ~/git/JeffersonLabWork/rawDataDir
+setenv JAVAPATH ~/git/JeffersonLabWork/javaFiles
+setenv CENTROIDPATH ~/git/JeffersonLabWork/centroidValuesDir
+setenv ELLIPSEPATH ~/git/JeffersonLabWork/ellipseDir
+setenv ELEGANTPATH ~/git/JeffersonLabWork/elegantPPSSDir
+setenv http_proxy https://jlab.org:8081
+setenv https_proxy https://jlab.org:8081
