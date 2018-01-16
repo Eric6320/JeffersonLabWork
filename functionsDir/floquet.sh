78d195066c755fa8e905be395c437eeab0438982
Eric6320 <erict@opsl00.acc.jlab.org>
2018-01-16 12:54:37 -0500

    commented floquet.sh

--- functionsDir/floquet.sh
+++ functionsDir/floquet.sh
@@ -1,27 +1,31 @@
 #!/bin/tcsh
 unset noclobber
 
-# TODO DONE
+#* Description: Performs either a floquet, or inverse floquet transformation on the data in the input file and saves it as the outputfile
+#* Argument: $1 - inputFile - Input file containing the coordinates to be transformed
+#* Argument: $2 - outputFile - Output file location used to store the transformed coordinates
+#* Argument: $3 - BPM - Name of the BPM to be used as a reference point for the transformation. BPM from which to pull Twiss information
+#* Argument: $4 - Verticle - 1 or 0 indicating whether to make verticle or horizontal considerations for the transformation
+#* Argument: inverse - If the word 'inverse' is included anywhere in the command line argument, the transformation will be an inverse floquet transformation, otherwise it will be a standard floquet transformation
 
 # Store argument variables
 set INPUTFILE = $1
 set OUTPUTFILE = $2
-set BPMONE = $3
+set BPM = $3
 set VERTICLE = $4
 
 set INVERSE = `echo $argv | grep -c "inverse"`
 
-#sdds2stream -col=s,ElementName,alphax,alphay,betax,betay,psix,psiy $DESIGNTWISSFILE >! information.twiasc
-
-setenv ALPHA `grep -w $BPMONE $RDPATH/information.twiasc | awk -v verticle=$VERTICLE '{print $(3+verticle)}'`
-setenv BETA  `grep -w $BPMONE $RDPATH/information.twiasc | awk -v verticle=$VERTICLE '{print $(5+verticle)}'`
-setenv EMITTANCE 1e-9
+# Store Twiss information from the given $BPM variable
+set ALPHA = `grep -w $BPM $RDPATH/information.twiasc | awk -v verticle=$VERTICLE '{print $(3+verticle)}'`
+set BETA = `grep -w $BPM $RDPATH/information.twiasc | awk -v verticle=$VERTICLE '{print $(5+verticle)}'`
+set EMITTANCE = 1e-9
 
 # Perform the appropriate transformation on the given data file
 if ($INVERSE == 1) then
 	# Perform Inverse Floquet Transformation to place the Unit Circle points onto a Betatron Ellipse
 	cat $INPUTFILE | awk -v Beta=$BETA -v Epsilon=$EMITTANCE -v Alpha=$ALPHA '{print $1*sqrt(Beta*Epsilon)" "sqrt(Epsilon/Beta) * ($2 + $1*Alpha)}' >! $OUTPUTFILE
 else
-	# Perform Inverse Floquet Transformation to place the Unit Circle points onto a Betatron Ellipse
+	# Perform a standard Floquet Transformation to turn the Betatron Ellipse back into a Unit Circle
 	cat $INPUTFILE | awk -v Beta=$BETA -v Epsilon=$EMITTANCE -v Alpha=$ALPHA '{print $1/sqrt(Beta*Epsilon)" "$2*sqrt(Beta/Epsilon) - (($1 * Alpha)/sqrt(Beta*Epsilon))}' >! $OUTPUTFILE
 endif
