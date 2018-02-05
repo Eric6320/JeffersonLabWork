49c7ce43a5b9c822e9f7a83e200fef23a2bd8b9a
Eric6320 <erict@opsl00.acc.jlab.org>
2018-02-05 17:09:43 -0500

    removed changeFunction.sh

---
+++ functionsDir/matrixCompile.sh
@@ -0,0 +1,26 @@
+#!/bin/tcsh
+unset noclobber
+
+#* Description:
+#* Argument: - 
+#* Argument: - 
+#* Argument: - 
+#* Argument: - 
+#* Argument: - 
+#* Example: 
+#* Further Comments: 
+#* Further Comments: 
+#* Main Output:
+
+# Set variables from command line arguments
+
+rm $CHANGEPATH/"response.dat" >& /dev/null
+touch "response.dat"
+foreach i (`sdds2stream -col=ElementName $DESIGNBEAMLINE.twi | grep "MQ"`)
+	cat "cDir$i/sumChi2DOF.dat" | awk -v bpm=$i '{print bpm" "$1}' >> "response.dat"
+end
+
+gedit "response.dat"
+
+M = np.loadtxt(str(matrixPath)+'matrixM.fin', delimiter=',')
+X = np.loadtxt(str(matrixPath)+'matrixX.fin', delimiter=',')
