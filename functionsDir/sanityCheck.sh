b7c6a62a1aaab2c957c1fa6362b3238ba759152a
Eric6320 <eric.thompson.13@cnu.edu>
2018-02-13 21:11:52 -0500

    Fully commented sanityCheck.sh

--- functionsDir/sanityCheck.sh
+++ functionsDir/sanityCheck.sh
@@ -1,14 +1,16 @@
 #!/bin/tcsh
 unset noclobber
 
-#* Description:
-#* Argument: $1 - 
-#* Argument: $2 - 
-#* Argument: $3+ - 
-#* Example: 
-#* Further Comments: 
-#* Further Comments: 
-#* Main Output:
+#* Description: Verifies that both floquet transformations executed within simulate.sh are accurate.
+#* Argument: $1 - BETATRONFILEONE - Data file containing the centroid value pairs for the unit circle - betatron ellipse floquet comparison
+#* Argument: $2 - BETATRONFILETWO - Data file containing the centroid value pairs for the betatron ellipse - modified betatron ellipse floquet comparison
+#* Argument: $3 - BPM - Reference BPM used for each floquet transformation
+#* Argument: $4 - N - Number of orbits to be included in the floquet transformations
+#* Argument: $5 - VERTICLE - Boolean 1 or 0 which represents whether the floquet transformation is verticle or horizontal
+#* Argument: PLOTCHECK - If the word 'plot' is included anywhere in the argument line, the betatron plots for each transformation will be shown
+#* Example: ./sanityCheck.sh modifiedEllipseOne.dat IPM1S03CentroidValues.dat IPM1S03 10 1 plot
+#* Further Comments: This script can only be run after both the initial and traced floquet transformations have been performed
+#* Main Output: Two printed chi2dof values representing the accuracy of both floquet transformations
 
 # Set variables from arguments
 set BETATRONFILEONE = $1
@@ -17,6 +19,7 @@ set BPM = $3
 set N = $4
 set VERTICLE = $5
 
+# Determine whether or not the command line arguments include the word 'plot'
 set PLOTCHECK = `echo $argv | grep -c "plot"`
 
 echo "sanityCheck.sh - Starting Floquet comparison / varification"
@@ -26,6 +29,7 @@ echo "sanityCheck.sh - Starting Floquet comparison / varification"
 $FPATH/floquet.sh $BETATRONFILEONE "modifiedUnitCircleOne.dat" $BPM $VERTICLE
 $FPATH/floquet.sh $BETATRONFILETWO "modifiedUnitCircleTwo.dat" $BPM $VERTICLE
 
+# If stated, plot the relevant floquet transformations
 if ($PLOTCHECK == 1) then
 	# Graph the origional image, the transformed unit circle, and the design unit circle
 	$FPATH/plot.sh $BETATRONFILE title=Initial Betatron Ellipse, xAxisLabel=X, yAxisLabel=xPrime,
