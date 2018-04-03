a155faba55ffaa8b2743e1f89f08f3aba9c53aca
Eric6320 <erict@opsl00.acc.jlab.org>
2018-04-03 10:53:48 -0400

    hopefully finished filter.sh, removed argument from pullS.sh

--- filter.sh
+++ filter.sh
@@ -1,12 +1,28 @@
 #!/bin/tcsh
 unset noclobber
 
+#Current CHI2DOF total: 3.11798 - Tolerance: 3.5
+#Final CHI2DOF sum: 3.11798, Trials required: 1
+
+rm $FINALPATH/*.fin > /dev/null
 foreach i (`ls $FINALPATH/*.dat`)
-	set QUADNAME = `echo $i | awk -F"-" '{print $1}'`
+	set QUADNAME = `echo $i | awk -F"-" '{print $1}' | awk -F"/" '{print $8}'`
 	set SEED = `echo $i | awk -F"-" '{print $2}' | awk -F"." '{print $1}'`
-	
-	grep -hE 'Final|\/\*|Current CHI2DOF total' $i >! 
+	set S = `$FPATH/pullS.sh $QUADNAME`
+
+	set CURRENT = `grep -m 1 'Current' $i | awk '{print $4}' | sed 's/,//g'`
+	set FINAL = `grep 'Final' $i | awk '{print $4}' | sed 's/,//g'`
+
+	echo "$S $QUADNAME $CURRENT $FINAL $SEED" | tee -a "$FINALPATH/finalData.fin"
 end
+
+gedit "$FINALPATH/finalData.fin"
+exit
+
+foreach i (`ls $FINALPATH/*.fin`)
+	cat $i >>! "$FINALPATH/finalData.fin"
+end
+
 
 exit
 
