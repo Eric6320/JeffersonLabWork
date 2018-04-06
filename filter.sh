3443d898b1bfc04f5ace13f760eff0914bc343ad
Eric Thompson <erict@satogata.jlab.org>
2018-04-06 09:15:14 -0400

    made pre and post minimization plots

--- filter.sh
+++ filter.sh
@@ -5,10 +5,14 @@ unset noclobber
 #Final CHI2DOF sum: 3.11798, Trials required: 1
 
 rm $FINALPATH/*.fin > /dev/null
+
 foreach i (`ls $FINALPATH/*.dat`)
-	set QUADNAME = `echo $i | awk -F"-" '{print $1}' | awk -F"/" '{print $8}'`
+	
+	set QUADNAME = `echo $i | awk -F"-" '{print $1}' | awk -F"/" '{print $7}'`
 	set SEED = `echo $i | awk -F"-" '{print $2}' | awk -F"." '{print $1}'`
 	set S = `$FPATH/pullS.sh $QUADNAME`
+
+	echo "$QUADNAME $SEED $S"
 
 	set CURRENT = `grep -m 1 'Current' $i | awk '{print $4}' | sed 's/,//g'`
 	set FINAL = `grep 'Final' $i | awk '{print $4}' | sed 's/,//g'`
@@ -18,8 +22,8 @@ end
 
 cd $FINALPATH
 
-gnuplot -persist -e "plot 'finalData.fin' using 1:3 with points title 'Pre-Minimized CHI2DOF Values'"
+gnuplot -persist -e "set xlabel 'S Coordinate (m)'; set ylabel 'CHI2DOF'; plot 'finalData.fin' using 1:3 with points title 'Pre-Minimization CHI2DOF Values'"
 
-gnuplot -persist -e "plot 'finalData.fin' using 1:4 with points title 'Minimized CHI2DOF Values'"
+gnuplot -persist -e "set xlabel 'S Coordinate (m)'; set ylabel 'CHI2DOF'; plot 'finalData.fin' using 1:4 with points title 'Post-Minimization CHI2DOF Values'"
 
 cd ..
